a224bfae38672e95c6d47f2e2906cc8f158cf1d7
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-09-06 22:03:47 +0100

    update ui

--- static/js/dashbroad.js
+++ static/js/dashbroad.js
@@ -2,8 +2,20 @@
 
 const btn = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const nav_header = document.getElementById("nav_profile");
 
 // add eventListener to button :
+
+nav_header.addEventListener("click",function(){
+  const element = document.querySelector(".user_services");
+  if(element.style.display === 'flex'){
+    element.style.display = 'none';
+  }else{
+    element.style.display = 'flex';
+  }
+});
+
+
 
 btn.addEventListener("click", function(){
   /*if(links.classList.contains("show-links")){
